72b46ff494936c39e90497bdc17a9a3e9b9ed58f
Fabian Yate <fabianyate31@gmail.com>
2026-01-31 23:46:58 -0500

    Add `DomainException` hierarchy for error handling and `EmailAlreadyInUseException` for email uniqueness validation

---
+++ src/main/java/com/fyr/finapp/domain/exception/messages/UserErrorMessages.java
@@ -0,0 +1,7 @@
+package com.fyr.finapp.domain.exception.messages;
+
+public final class UserErrorMessages {
+    private UserErrorMessages(){}
+
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
+}
