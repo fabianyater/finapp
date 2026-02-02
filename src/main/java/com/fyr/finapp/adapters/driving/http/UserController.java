4b95095d9f06e27a53d9f8d91aada5105db65800
Fabian Yate <fabianyate31@gmail.com>
2026-02-01 20:28:35 -0500

    Use `201 Created` response with `Location` header in `create` endpoint of `UserController`

--- src/main/java/com/fyr/finapp/adapters/driving/http/UserController.java
+++ src/main/java/com/fyr/finapp/adapters/driving/http/UserController.java
@@ -5,12 +5,14 @@ import com.fyr.finapp.adapters.driving.http.dto.CreateUserResponse;
 import com.fyr.finapp.domain.api.user.CreateUserUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +21,10 @@ class UserController {
     private final CreateUserUseCase createUserUseCase;
 
     @PostMapping
-    public ResponseEntity<CreateUserResponse> create(@Valid @RequestBody CreateUserRequest request) {
+    public ResponseEntity<CreateUserResponse> create(
+            @Valid
+            @RequestBody
+            CreateUserRequest request) {
         var command = new CreateUserUseCase.CreateUserCommand(
                 request.name(),
                 request.surname(),
@@ -30,7 +35,14 @@ class UserController {
         var result = createUserUseCase.create(command);
 
         var response = new CreateUserResponse(result.userId(), result.email());
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(result.userId())
+                .toUri();
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity
+                .created(location)
+                .body(response);
     }
 }
