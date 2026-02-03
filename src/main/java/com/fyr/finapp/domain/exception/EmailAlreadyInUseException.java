79b866d937935ce1f85d4e3fcb132017b36d7b5f
Fabian Yate <fabianyate31@gmail.com>
2026-02-02 23:52:44 -0500

    Remove unused error handling classes and tests for domain exceptions
    - Deleted `ErrorCode` enum, `EmailAlreadyInUseException`, and `CurrencyCode` value object along with related test cases.

--- src/main/java/com/fyr/finapp/domain/exception/EmailAlreadyInUseException.java
+++
@@ -1,7 +0,0 @@
-package com.fyr.finapp.domain.exception;
-
-public class EmailAlreadyInUseException extends DomainException {
-    public EmailAlreadyInUseException(String message, ErrorCode code) {
-       super(message, code);
-    }
-}
