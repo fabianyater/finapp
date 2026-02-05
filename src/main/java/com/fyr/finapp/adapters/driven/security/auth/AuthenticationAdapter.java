e9cee1f961a3af9e181add2883782189319d454e
Fabian Yate <fabianyate31@gmail.com>
2026-02-04 22:47:47 -0500

    Add `getCurrentUserId` method to `IAuthenticationRepository` and implement in `AuthenticationAdapter`
    - Extended `IAuthenticationRepository` to include `getCurrentUserId` for retrieving the authenticated user's ID.
    - Implemented the method in `AuthenticationAdapter` using Spring Security's `SecurityContextHolder`.

--- src/main/java/com/fyr/finapp/adapters/driven/security/auth/AuthenticationAdapter.java
+++ src/main/java/com/fyr/finapp/adapters/driven/security/auth/AuthenticationAdapter.java
@@ -5,11 +5,13 @@ import com.fyr.finapp.adapters.driven.security.user.SecurityUser;
 import com.fyr.finapp.domain.api.auth.AuthenticateUseCase;
 import com.fyr.finapp.domain.model.user.vo.Email;
 import com.fyr.finapp.domain.model.user.vo.PlainPassword;
+import com.fyr.finapp.domain.model.user.vo.UserId;
 import com.fyr.finapp.domain.spi.auth.IAuthenticationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Objects;
 
@@ -28,5 +30,21 @@ public class AuthenticationAdapter implements IAuthenticationRepository {
         String token = jwtProvider.generateToken(Objects.requireNonNull(principal));
 
         return new AuthenticateUseCase.AuthResult(token, principal.getId().toString(), principal.getUsername());
+    }
+
+    @Override
+    public UserId getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new IllegalStateException("No authenticated user found");
+        }
+
+        SecurityUser userDetails = Objects.requireNonNull((SecurityUser) authentication.getPrincipal());
+        var id = Objects.requireNonNull(userDetails.getId());
+
+        return UserId.of(id.toString());
     }
 }
--- src/main/java/com/fyr/finapp/domain/spi/auth/IAuthenticationRepository.java
+++ src/main/java/com/fyr/finapp/domain/spi/auth/IAuthenticationRepository.java
@@ -3,7 +3,9 @@ package com.fyr.finapp.domain.spi.auth;
 import com.fyr.finapp.domain.api.auth.AuthenticateUseCase;
 import com.fyr.finapp.domain.model.user.vo.Email;
 import com.fyr.finapp.domain.model.user.vo.PlainPassword;
+import com.fyr.finapp.domain.model.user.vo.UserId;
 
 public interface IAuthenticationRepository {
     AuthenticateUseCase.AuthResult authenticate(Email email, PlainPassword password);
+    UserId getCurrentUserId();
 }
