1870efad7ff19b21f6b4a08c2feef13137118526
Fabian Yate <fabianyate31@gmail.com>
2026-02-08 19:06:40 -0500

    Remove unused pageable method from `AccountJpaRepository`.

--- src/main/java/com/fyr/finapp/adapters/driven/persistence/jpa/repository/AccountJpaRepository.java
+++ src/main/java/com/fyr/finapp/adapters/driven/persistence/jpa/repository/AccountJpaRepository.java
@@ -1,8 +1,6 @@
 package com.fyr.finapp.adapters.driven.persistence.jpa.repository;
 
 import com.fyr.finapp.adapters.driven.persistence.jpa.entity.AccountEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -12,7 +10,4 @@ public interface AccountJpaRepository extends
         JpaRepository<AccountEntity, UUID>,
         JpaSpecificationExecutor<AccountEntity> {
     boolean existsByUser_IdAndName(UUID id, String name);
-
-    Page<AccountEntity> findByUser_Id(UUID id, Pageable pageable);
-
 }
