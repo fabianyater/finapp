f6d8f06f46eb852634088008887f8497584f4a86
Fabian Yate <fabianyate31@gmail.com>
2026-02-09 23:55:06 -0500

    Refactor `UpdateAccountService` to integrate `AccountValidator` for centralized validation logic

--- src/main/java/com/fyr/finapp/application/usecase/account/UpdateAccountService.java
+++ src/main/java/com/fyr/finapp/application/usecase/account/UpdateAccountService.java
@@ -4,11 +4,7 @@ import com.fyr.finapp.domain.api.account.UpdateAccountUseCase;
 import com.fyr.finapp.domain.common.vo.Color;
 import com.fyr.finapp.domain.common.vo.Icon;
 import com.fyr.finapp.domain.common.vo.Money;
-import com.fyr.finapp.domain.exception.ForbiddenException;
-import com.fyr.finapp.domain.exception.NotFoundException;
-import com.fyr.finapp.domain.exception.ValidationException;
 import com.fyr.finapp.domain.model.account.Account;
-import com.fyr.finapp.domain.model.account.exception.AccountErrorCode;
 import com.fyr.finapp.domain.model.account.vo.AccountId;
 import com.fyr.finapp.domain.model.account.vo.AccountName;
 import com.fyr.finapp.domain.model.account.vo.AccountType;
@@ -16,16 +12,17 @@ import com.fyr.finapp.domain.model.user.vo.UserId;
 import com.fyr.finapp.domain.spi.account.IAccountRepository;
 import com.fyr.finapp.domain.spi.auth.IAuthenticationRepository;
 import jakarta.transaction.Transactional;
-import org.jspecify.annotations.NonNull;
 
 public class UpdateAccountService implements UpdateAccountUseCase {
     private final IAccountRepository accountRepository;
     private final IAuthenticationRepository authenticationRepository;
+    private final AccountValidator accountValidator;
 
     public UpdateAccountService(IAccountRepository accountRepository,
-                                IAuthenticationRepository authenticationRepository) {
+                                IAuthenticationRepository authenticationRepository, AccountValidator accountValidator) {
         this.accountRepository = accountRepository;
         this.authenticationRepository = authenticationRepository;
+        this.accountValidator = accountValidator;
     }
 
     @Override
@@ -34,11 +31,11 @@ public class UpdateAccountService implements UpdateAccountUseCase {
         var accountId = AccountId.of(command.accountId());
         var userId = authenticationRepository.getCurrentUserId();
 
-        Account existingAccount = getAccountOrThrow(accountId);
-        validateAccountOwnership(existingAccount, userId);
+        Account existingAccount = accountValidator.getAccountAndValidateOwnership(accountId, userId);
 
         var newName = AccountName.of(command.name());
-        validateUniqueNameIfChanged(existingAccount, newName, userId);
+
+        accountValidator.validateUniqueNameForUpdate(existingAccount, newName, userId);
 
         handleDefaultAccountChange(command, existingAccount, userId);
         updateExcludeFromTotalIfNeeded(command, existingAccount);
@@ -54,34 +51,6 @@ public class UpdateAccountService implements UpdateAccountUseCase {
         );
 
         accountRepository.save(existingAccount);
-    }
-
-    private @NonNull Account getAccountOrThrow(AccountId accountId) {
-        return accountRepository.findById(accountId)
-                .orElseThrow(() -> new NotFoundException(
-                        "Account not found for id=" + accountId,
-                        AccountErrorCode.ACCOUNT_NOT_FOUND
-                ));
-    }
-
-    private void validateAccountOwnership(Account existingAccount, UserId userId) {
-        if (!existingAccount.getUserId().equals(userId)) {
-            throw new ForbiddenException(
-                    "You don't have access to this account",
-                    AccountErrorCode.ACCESS_DENIED
-            );
-        }
-    }
-
-    private void validateUniqueNameIfChanged(Account existingAccount, AccountName newName, UserId userId) {
-        if (existingAccount.getName().equals(newName)) return;
-
-        if (accountRepository.existsByUserIdAndName(userId, newName)) {
-            throw new ValidationException(
-                    "An account with this name already exists",
-                    AccountErrorCode.NAME_ALREADY_EXISTS
-            );
-        }
     }
 
     private void handleDefaultAccountChange(Command command, Account existingAccount, UserId userId) {
