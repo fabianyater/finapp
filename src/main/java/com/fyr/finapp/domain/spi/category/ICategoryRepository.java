dfc13312050328a652e018533298094e7bfab332
Fabian Yate <fabianyate31@gmail.com>
2026-02-11 22:24:33 -0500

    Add CreateCategoryUseCase interface and ICategoryRepository for category management

---
+++ src/main/java/com/fyr/finapp/domain/spi/category/ICategoryRepository.java
@@ -0,0 +1,13 @@
+package com.fyr.finapp.domain.spi.category;
+
+import com.fyr.finapp.domain.model.category.Category;
+import com.fyr.finapp.domain.model.category.vo.CategoryName;
+import com.fyr.finapp.domain.model.user.vo.UserId;
+import com.fyr.finapp.domain.shared.vo.TransactionType;
+
+public interface ICategoryRepository {
+    void save(Category category);
+    void saveAll(Iterable<Category> categories);
+
+    boolean existsByUserIdAndTypeAndName(UserId userId, TransactionType type, CategoryName name);
+}
