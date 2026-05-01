98a334d69cf2602f3fb9fbf0a953ce027c4a5aad
Fabian Yate <fabianyate31@gmail.com>
2026-05-01 01:03:06 -0500

    fix: update cron schedule for recurring transaction trigger

--- src/main/java/com/fyr/finapp/application/usecase/recurring/RecurringTransactionScheduler.java
+++ src/main/java/com/fyr/finapp/application/usecase/recurring/RecurringTransactionScheduler.java
@@ -11,7 +11,7 @@ public class RecurringTransactionScheduler {
         this.processService = processService;
     }
 
-    @Scheduled(cron = "0 0 1 * * *")
+    @Scheduled(cron = "0 10 1 * * *")
     public void trigger() {
         processService.process();
     }
